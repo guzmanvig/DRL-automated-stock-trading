5c6aa740da62235a8113ff0230f7e78b149f947b
Sana Jahan <sanadgrt@gmail.com>
2020-12-07 00:31:50 -0800

    WIP: Added script to read doc file and plot graph, added tesla share which has latest data till 2020 December

--- visualize-reward.py
+++ visualize-reward.py
@@ -1,72 +1,44 @@
 import docx
-
-doc = docx.opendocx("./logs/currency_run_1.docx")
-a2ctext = open('./results/a2c-logs-results.txt', 'w')
-ddpgtext = open('./results/ddpg-logs-results.txt', 'w')
-ppotext = open('./results/ppo-logs-results.txt', 'w')
-all_paras = docx.getdocumenttext(doc)
-a2c, ddpg, ppo = False, False, False
+import matplotlib.pyplot as plt
 
 
 class DocReader:
 
-    def __init__(self):
+    def __init__(self, doc_path):
         print("Started writing")
-
-    def get_algorithm(self, algo):
-        switcher = {
-            0: b'A2C Training',
-            1: b'PPO Training',
-            2: b'DDPG Training'
-        }
-        return switcher.get(algo, "Invalid algorithm")
+        self.doc = docx.opendocx(doc_path)
+        self.all_paras = docx.getdocumenttext(self.doc)
 
     def write_txt(self):
-        for para_text in all_paras:
-            line = para_text.encode("utf-8")
-            if self.get_algorithm(0) in line:
-                a2c, ddpg, ppo = True, False, False
-            elif self.get_algorithm(1) in line:
-                a2c, ddpg, ppo = False, False, True
-            elif self.get_algorithm(2) in line:
-                a2c, ddpg, ppo = False, True, False
-            else:
-                print(line)
-
-            if b'total_reward' in line:
-                if a2c:
-                    a2ctext.write('\n\n'.join(line))
-                    # docx.savedocx(a2ctext)
-                elif ddpg:
-                    print(line)
-                    ddpgtext.write('\n\n'.join(str(line)))
-                elif ppo:
-                    ppotext.write('\n\n'.join(line))
-        a2c, ddpg, ppo = False, False, False
-
-        # Create our properties, contenttypes, and other support files
-        title = 'Python docx demo'
-        subject = 'A practical example of making docx from Python'
-        creator = 'Mike MacCana'
-        keywords = ['python', 'Office Open XML', 'Word']
-
-        coreprops = docx.coreproperties(title=title, subject=subject, creator=creator,
-                                        keywords=keywords)
-        appprops = docx.appproperties()
-        contenttypes = docx.contenttypes()
-        websettings = docx.websettings()
-        wordrelationships = docx.wordrelationships(None)
-
-        docx.savedocx(a2ctext, coreprops, appprops, contenttypes, websettings,
-                      wordrelationships, 'Welcome to the Python docx module.docx')
-        docx.savedocx(ddpgtext, coreprops, appprops, contenttypes, websettings,
-                      wordrelationships, 'Welcome to the Python docx module.docx')
-        docx.savedocx(ppotext, coreprops, appprops, contenttypes, websettings,
-                      wordrelationships, 'Welcome to the Python docx module.docx')
+        date_from_list = []
+        date_to_list = []
+        reward_list = []
+        initial_asset = 1000000
+        for para_text in self.all_paras:
+            line = para_text
+            if 'Trading from: ' in line:
+                trade_line = ' '.join(line.split())
+                date_from = trade_line.split(" ")[2]
+                date_to = trade_line.split(" ")[4]
+                date_from = date_from[:4] + "-" + date_from[4:6] + "-" + date_from[6:8]
+                date_to = date_to[:4] + "-" + date_to[4:6] + "-" + date_to[6:8]
+                date_from_list.append(date_from)
+                date_to_list.append(date_to)
+                print(date_from + " " + date_to + " ")
+            if 'end_total_asset' in line:
+                end_asset = int(line.split(":")[1].split(".")[0])
+                reward = abs(end_asset - initial_asset) * 100 / initial_asset
+                reward_list.append(reward)
+                print(reward)
+        print(date_from_list)
+        print(date_to_list)
+        print(reward_list)
+        plt.plot(date_to_list, reward_list)
+        plt.show()
 
 
 def __main__():
-    doc_reader = DocReader()
+    doc_reader = DocReader("./logs/currency_run_1.docx")
     doc_reader.write_txt()
 
 
